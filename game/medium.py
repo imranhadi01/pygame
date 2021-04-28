25e5c95c9c9f0a3152df66298d9a3730fe3df50d
Muhammad imran <68772252+imranhadi01@users.noreply.github.com>
2021-04-28 13:52:50 +0500

    Update medium.py

--- game/medium.py
+++ game/medium.py
@@ -5,7 +5,6 @@ def randomApi():
     r = requests.get('https://www.random.org/integers/?num=5&min=0&max=7&col=1&base=10&format=plain&rnd=new')
     number=r.text.replace('\n',"")
     return number
-
 def medium():
     random= randomApi()
     attempts = 5
